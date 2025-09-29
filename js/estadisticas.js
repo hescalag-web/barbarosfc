49c87c951ab6f9eaea38745c6b6d69acc9e5719b
hescalag <hernan.escalantegarcia@cencosud.cl>
2025-09-29 18:29:40 -0300

    hescalag fix plantel-galeria

---
+++ js/estadisticas.js
@@ -0,0 +1,40 @@
+console.log("Sitio Bárbaros FC cargado");
+
+const estadisticas = [
+  { 
+    "fecha": "2025-09-30", 
+    "rival": "Tigres FC", 
+    "resultado": "2-1",
+    "lugar": "Taqueral"
+  },
+  {
+    "fecha": "2025-10-07", 
+    "rival": "Leones FC", 
+    "resultado": "-",
+    "lugar": "Taqueral"
+  },
+  {
+    "fecha": "2025-10-07", 
+    "rival": "Leones FC", 
+    "resultado": "-",
+    "lugar": "Taqueral"
+  },
+  {
+    "fecha": "2025-10-07", 
+    "rival": "Leones FC", 
+    "resultado": "-",
+    "lugar": "Taqueral"
+  },
+  {
+    "fecha": "2025-10-07", 
+    "rival": "Leones FC", 
+    "resultado": "-",
+    "lugar": "Taqueral"
+  },
+  {
+    "fecha": "2025-10-07", 
+    "rival": "Leones FC", 
+    "resultado": "-",
+    "lugar": "Taqueral"
+  }
+]
--- js/ultimo_encuentros.js
+++ js/ultimo_encuentros.js
@@ -2,39 +2,63 @@ console.log("Sitio Bárbaros FC cargado");
 
 const encuentros = [
   { 
-    "fecha": "2025-09-30", 
-    "rival": "Tigres FC", 
-    "resultado": "2-1",
-    "lugar": ""
+    "fecha": "2025-09-29", 
+    "rival": "Ases del Balón", 
+    "resultado": "-",
+    "lugar": "Taqueral"
   },
   {
-    "fecha": "2025-10-07", 
-    "rival": "Leones FC", 
-    "resultado": "-",
-    "lugar": ""
+    "fecha": "2025-09-08", 
+    "rival": "Unión sin Fronteras", 
+    "resultado": "13-6-",
+    "lugar": "Soccer Siete"
   },
   {
-    "fecha": "2025-10-07", 
-    "rival": "Leones FC", 
-    "resultado": "-",
-    "lugar": ""
+    "fecha": "2025-09-01", 
+    "rival": "Academia", 
+    "resultado": "3-2",
+    "lugar": "Soccer Siete"
   },
   {
-    "fecha": "2025-10-07", 
-    "rival": "Leones FC", 
-    "resultado": "-",
-    "lugar": ""
+    "fecha": "2025-08-25", 
+    "rival": "Forasteros", 
+    "resultado": "5-11",
+    "lugar": "Soccer Siete"
   },
   {
-    "fecha": "2025-10-07", 
-    "rival": "Leones FC", 
-    "resultado": "-",
-    "lugar": ""
+    "fecha": "2025-08-18", 
+    "rival": "San Lucho", 
+    "resultado": "5-6",
+    "lugar": "Soccer Siete"
   },
   {
-    "fecha": "2025-10-07", 
-    "rival": "Leones FC", 
-    "resultado": "-",
-    "lugar": ""
+    "fecha": "2025-08-15", 
+    "rival": "Suéltala", 
+    "resultado": "4-3",
+    "lugar": "Taqueral"
+  },
+  {
+    "fecha": "2025-08-11", 
+    "rival": "Sin Perdón", 
+    "resultado": "5-8",
+    "lugar": "Soccer Siete"
+  },
+  {
+    "fecha": "2025-08-04", 
+    "rival": "Nacimiento", 
+    "resultado": "2-4",
+    "lugar": "Soccer Siete"
+  },
+  {
+    "fecha": "2025-07-30", 
+    "rival": "Basuritas", 
+    "resultado": "9-3",
+    "lugar": "Taqueral"
+  },
+  {
+    "fecha": "2025-07-28", 
+    "rival": "Albacity", 
+    "resultado": "3-10",
+    "lugar": "Soccer Siete"
   }
 ]
